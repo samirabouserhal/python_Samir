a647bd07224bd499f9e4466cdb04f38e1e48e63c
unknown <cecce@FRCLOC203-67394.ecolecatholique.ca>
2020-11-24 09:37:15 -0500

    having fun

--- Workspace_Samir/projet.py
+++ Workspace_Samir/projet.py
@@ -1,7 +1,8 @@
-print("Doing this to configure run button")
-print("This is fun")
-x=3422423453
-y=4673262386
-print (max(x,y))
-print (min(x,y))
-print("This is a test to see if i can push and pull things from VS code to Repl.it")
+#print("Doing this to configure run button")
+#print("This is fun")
+x=867657678
+y=545645645
+#print (max(x,y))
+#print (min(x,y))
+#print("This is a test to see if i can push and pull things from VS code to Repl.it")
+print((max(x*y,x*x))
