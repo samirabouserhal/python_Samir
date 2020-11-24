87367565a5b8b272d31ac41eafe203694ed8e3a7
unknown <cecce@FRCLOC203-67394.ecolecatholique.ca>
2020-11-24 09:39:13 -0500

    test

--- Workspace_Samir/projet.py
+++ Workspace_Samir/projet.py
@@ -1,8 +1,1 @@
-#print("Doing this to configure run button")
-#print("This is fun")
-x=867657678
-y=545645645
-#print (max(x,y))
-#print (min(x,y))
-#print("This is a test to see if i can push and pull things from VS code to Repl.it")
-print((max(x*y,x*x))
+print("This is a test")
