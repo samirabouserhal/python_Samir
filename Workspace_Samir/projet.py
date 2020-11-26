24e9e26640347764e84133f354985a9558ed3fbd
unknown <cecce@FRCLOC203-67394.ecolecatholique.ca>
2020-11-26 13:04:22 -0500

    That sequence of text was to test my ability to implement quotation markes and apostrophies in texts

--- Workspace_Samir/projet.py
+++ Workspace_Samir/projet.py
@@ -1,2 +1,5 @@
 #Lieu de travail principal de Samir Abou Serhal
-#print("This is a test")
+#print("This is a test to see if single \' and \" will work now ")
+#print('Now let\'s see what the # does')
+#print("Victor then said,\"The sky is blue\".")
+# That sequence of text was to test my ability to implement quotation markes and apostrophies in texts
