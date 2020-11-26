54fe2da349ab86091156d72a1e3227fb36cc7054
Samir Abou-Serhal <abosam30@ecolecatholique.ca>
2020-11-26 18:54:50 +0000

    la date

--- Formatif1/Hello_Samir.py
+++ Formatif1/Hello_Samir.py
@@ -1,2 +1,2 @@
-# Ceci est le fichier de travail de Samir Abou Serhal pour le projet Hello World submit le 25 Novembre 2020.
+# Ceci est le fichier de travail de Samir Abou Serhal pour le projet Hello World soumit le 25 Novembre 2020.
 print("Hello World!")
