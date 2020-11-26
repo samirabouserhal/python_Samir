a7b90c08fd9ff3d8a0679d31c256515932fbac2a
unknown <cecce@FRCLOC203-67394.ecolecatholique.ca>
2020-11-26 14:35:11 -0500

    changes

--- Workspace_Samir/Test_space.py
+++ Workspace_Samir/Test_space.py
@@ -4,8 +4,11 @@
 #print("Victor then said,\"The sky is blue\".")
 # That sequence of text was to test my ability to implement quotation markes and apostrophies in texts
 #print("")
-x= 465455454658457685468465843575
-y=54545343543546545456657688768
-z= x+y
+#x= 465455454658457685468465843575
+#y=54545343543546545456657688768
+#z= x+y
 #print(z)
-print(max(x,y))
+#print(max(x,y))
+print("Ask the computer to repeat something")
+user_input=input()
+print("The computer has processed your request, it has decided to repeat:",user_input)
