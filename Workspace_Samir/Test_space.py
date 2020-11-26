a385800837f6f4ddfc30a51ce81995f6967aa9ed
unknown <cecce@FRCLOC203-67394.ecolecatholique.ca>
2020-11-26 14:38:11 -0500

    changes

--- Workspace_Samir/Test_space.py
+++ Workspace_Samir/Test_space.py
@@ -1,14 +1,3 @@
-#Lieu de travail principal de Samir Abou Serhal
-#print("This is a test to see if single \' and \" will work now ")
-#print('Now let\'s see what the # does')
-#print("Victor then said,\"The sky is blue\".")
-# That sequence of text was to test my ability to implement quotation markes and apostrophies in texts
-#print("")
-#x= 465455454658457685468465843575
-#y=54545343543546545456657688768
-#z= x+y
-#print(z)
-#print(max(x,y))
 print("Ask the computer to repeat something")
 user_input=input()
 print("The computer has processed your request, it has decided to repeat:",user_input)
