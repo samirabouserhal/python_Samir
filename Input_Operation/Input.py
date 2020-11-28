a4fe6e203418f6763a82f70835aebbd9b6fbafe0
Samir Abou-Serhal <abosam30@ecolecatholique.ca>
2020-11-28 19:44:18 +0000

    updated

--- Input_Operation/Input.py
+++ Input_Operation/Input.py
@@ -1,9 +1,10 @@
-print("Bonjour, je suis une calculatrice mathématique interactive. Pouvez-vous me donnez votre nom pour que s'avoir comment vous appeler?")
+#Ceci est le lieu de travail de Samir Abou Serhal pour le travail d'input et opération. Ce fichier fut créer le 28 Novembre 2020 pour le cours ICS3UA-01/51.
+print("Bonjour, je suis une calculatrice mathématique interactive. Pouvez-vous me donnez votre nom pour que je puisse s'avoir comment vous appelez?")
 nom= input()
-print("Merci", nom,",vous pouvez maintenant insérer deux nombre de n'importe type pour les multiplier ensemble." )
+print("Merci", nom,",vous pouvez maintenant insérer deux nombres de n'importe quel type pour les multiplier ensemble." )
 x=input()
 y=input()
 float_x=float(x)
 float_y=float(y)
 print("La réponse de votre multiplication est,",float_x*float_y)
-print("Merci,", nom, "et bonne journée")
+print("Merci,", nom, "et bonne journée.")
