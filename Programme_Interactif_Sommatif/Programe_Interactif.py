3fd9c6bd1c05691579cab0f1c6151581779eabd0
unknown <cecce@FRCLOC203-67394.ecolecatholique.ca>
2020-12-03 11:29:39 -0500

    Commencer

--- Programme_Interactif_Sommatif/Programe_Interactif.py
+++ Programme_Interactif_Sommatif/Programe_Interactif.py
@@ -1,1 +1,72 @@
-# Ceci est le lieu de travail de Samir Abou Serhal pour son travail sommatif 
+# Ceci est le lieu de travail de Samir Abou Serhal pour son travail sommatif de programme interactif
+# Ce document fut créer le 3 décembre 2020 et modifier le 6 décembre 2020
+# Pour me contacter, envoyer un couriel à abosam@ecolecatholique.ca
+
+print("S'il vous plaît insérer votre nom:")
+nom= input()
+print("Bonjour", nom,", j'ai besoin de votre aide. La princess du royaume Guimauve, ma chère fille, fut kidnappé et vous êtes la seul personne qui puisse la sauvé.") 
+print("Lors de cette aventure, je vous présentera des choix qui se retrouveront entre guillemets.")
+print("Cette aventure sera difficile, mais sa serait un petit défi pour un chevalier comme vous.")
+print("")
+print("Voici votre premier choix")
+print("Accepter vous la mission de sauvé la princess du royaume des guimauves, 'oui' ou 'non'.")
+
+
+# Code qui décide si l'aventure commence ou non
+
+choix1=input().lower()
+if choix1=="oui":
+    estOui= True
+    print("")
+    print("Merci beaucoup", nom, "je vous récompensera pour votre bravoure plus-tard.") 
+    print("Le Mage Gandalf vous accompagnera sur votre aventure pour vous donner des conseils.")
+elif choix1=="non":
+    print("")
+    print("Je ne comprends pas, voulez vous laiser la princess mourir toute seule? 'oui' ou 'non'")
+    choix2=input().lower()
+    if choix2=="oui":
+        estOui=False
+        print("")
+        print("Vous êtes bani du royaume des guimauves. Si je vous vois encore, vous serez éxécuter")
+        print("")
+        print("GAME OVER")
+    elif choix2=="non":
+        estOui=True
+        print("")
+        print("Donc vous allez sauvé la princess?", nom,)
+else: 
+    print("Je m'excuse, je n'ai pas compris ce que vous avez écris." "Je présume que votre réponse est non.")
+    estOui=False
+    print("")
+    print("GAME OVER")
+if estOui==True:
+    print("Merci d'avoir choisi de sauver la princess. Votre aventure commence maintenant.")
+    print("")
+    print("Pèse enter pour continuer")
+    Pour_continuer=input()
+
+
+
+# Premier problème
+
+if estOui==True:
+    print("")
+    print("La princess fut transporté vers le royaune de Kit Kat au sud du royaume de Guimauve.")
+    print("Le Mage Gandalf est très familier avec se royaume et il te guidera vers le chateau où ma cher fille est détenu.")
+    print("")
+    print("Bonjour"', nom, '"Je suis le Mage Gandalf. J\'aimerais quité avant le couché du soleil, donc alons-y!")
+    print("Le Soleil se couche, on devrait se reposer la nuit")
+    print("Le Mage et", nom,"s'endorme autour du feu de camp que le mage a allumer")
+    print("")
+    print("Pèse enter pour continuer")
+    Pour_continuer=input()
+
+    print(nom.upper(), "RÉVEILLE-TOI, ON SE FAIT ATTACKER PAR DES GOBLINS!!!")
+    print(nom.upper()"LES GOBLINS ONT PEUR DU FEU. PREND UNE BRANCHE DU FEU DE CAMP POUR NOUS DÉFENDRE")
+    print("Qu'est-ce que vous aller faire, allez vous prendre le 'feu' ou aller vous sortir votre 'épée'")
+    choix3=input.lower()
+    
+    if choix3=="feu":
+
+
+
