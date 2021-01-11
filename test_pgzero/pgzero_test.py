23b061e699201d71ee5962d51263f93bf7b71cb4
Samir Abou-Serhal <abosam30@ecolecatholique.ca>
2021-01-11 22:32:28 +0000

    working on pgzero animation

--- test_pgzero/pgzero_test.py
+++ test_pgzero/pgzero_test.py
@@ -1,6 +1,19 @@
 import pgzrun
 
+WIDTH= 700
+HEIGHT= 500
+
+vert=(50, 220, 100)
+
+ballon= Actor("ballon.png")
+ballon.pos=(WIDTH / 2, HEIGHT / 2)
+
+joueur= Actor("joueur.png")
+joueur.pos=(WIDTH / 2, HEIGHT / 2)
+
 def draw():
-    screen.fill("cyan")
+  screen.fill(vert)
+  ballon.draw()
+  joueur.draw()
     
 pgzrun.go()
