1df1c0267adbc61f4247d2159986ef00c403bc90
Samir Abou-Serhal <abosam30@ecolecatholique.ca>
2021-01-11 22:35:36 +0000

    tweaking

--- test_pgzero/pgzero_test.py
+++ test_pgzero/pgzero_test.py
@@ -4,16 +4,20 @@ WIDTH= 700
 HEIGHT= 500
 
 vert=(50, 220, 100)
+speed = 1
 
 ballon= Actor("ballon.png")
-ballon.pos=(WIDTH / 2, HEIGHT / 2)
+ballon.pos=(WIDTH / 2, 700)
 
 joueur= Actor("joueur.png")
-joueur.pos=(WIDTH / 2, HEIGHT / 2)
+joueur.pos=(WIDTH / 2, 620)
 
 def draw():
   screen.fill(vert)
   ballon.draw()
   joueur.draw()
+
+def update():
+  ballon.x = ballon.x + speed
     
 pgzrun.go()
