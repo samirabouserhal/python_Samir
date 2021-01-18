bd59e6f6c929c6ba7ced8b91fe258ebaa2c5eea8
Samir Abou-Serhal <abosam30@ecolecatholique.ca>
2021-01-18 23:23:35 +0000

    tweaked

--- Projet_Pgzero/Projet_Pgzero.py
+++ Projet_Pgzero/Projet_Pgzero.py
@@ -4,18 +4,27 @@ import pgzrun
 WIDTH= 700
 HEIGHT= 500
 
-# add cropped and right target images
-# add arcade shooting range backgroud
-l_target = Actor("target.jpg")
-l_target.pos=(10,10)
+speed=2
 
-m_target = Actor("half-sized_target.jpg")
+l_target = Actor("l_target.png")
+l_target.pos=(WIDTH/2, HEIGHT/2)
+
+m_target = Actor("m_target.png")
 m_target.pos=(50,50)
+
+s_target = Actor("s_target.png")
+s_target.pos=(100,100)
+
 
 def draw():
   screen.fill("white")
   l_target.draw()
   m_target.draw()
+  s_target.draw()
 
-
+def update():
+  global speed
+  l_target.y= l_target.y + speed
+  if l_target.y < 0 or l_target.y > WIDTH:
+    speed=-speed
 pgzrun.go()
